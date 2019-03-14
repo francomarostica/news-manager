3343bac9e820ba30a3ed7c21f31511cf6a7c1149
fmarostica <franco-marostica@outlook.com>
2019-03-14 18:44:28 -0300

    Updated Main UI

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,59 +1,57 @@
-<header>
-    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-        <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('images/logo.svg') }}" class="logo" alt="" />
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="{{ asset('images/logo.svg') }}" class="logo" alt="" />
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
-                    
-                </ul>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                
+            </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('auth.login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('auth.login') }}</a>
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('auth.register') }}</a>
                         </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('auth.register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('panel') }}">
-                                    {{ __('auth.dashboard') }}
-                                </a>
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    {{ __('auth.logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="secondary-nav">
-        <div class="container">
-            @foreach ($categories as $category)
-                <a href="/{{ $category->url }}" class="category-mnu-item {{ ($currentCategory == $category->url) ? 'active' : '' }}">{{ $category->title }}</a>
-            @endforeach
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('panel') }}">
+                                {{ __('auth.dashboard') }}
+                            </a>
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                {{ __('auth.logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
         </div>
     </div>
-</header>
+</nav>
+<div class="secondary-nav">
+    <div class="container">
+        @foreach ($categories as $category)
+            <a href="/{{ $category->url }}" class="category-mnu-item {{ ($currentCategory == $category->url) ? 'active' : '' }}">{{ $category->title }}</a>
+        @endforeach
+    </div>
+</div>
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -1,16 +1,142 @@
 <!DOCTYPE html>
     <html lang="es">
     <head>
-        @include('layouts.head')
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>NewsManager - @yield('title')</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/breaking-news-ticker.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/breaking-news-ticker.min.js') }}"></script>
+        <script src="{{ asset('js/main.js') }}"></script>
     </head>
     <body>
-        @include('layouts.header')
-        @yield('content')
+        <header>
+            @include('layouts.header')
+        </header>
+        <section id="MainSection">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <article class="articlebox primary">
+                            <a href="#">
+                                <img class="image" src="/images/article_samples/1.jpg" />
+                            </a>
+                            <div class="titlebox">
+                                <div class="category">Article Category</a></div>
+                                <h1 class="title">Article Title</h1>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <article class="articlebox medium">
+                                    <a href="#">
+                                        <img class="image" src="/images/article_samples/2.jpg" />
+                                    </a>
+                                    <div class="titlebox">
+                                        <div class="category">Article Category</a></div>
+                                        <h1 class="title">Article Title</h1>
+                                    </div>
+                                </article>
+                            </div>
+                            <div class="col-md-6">
+                                <article class="articlebox medium">
+                                    <a href="#">
+                                        <img class="image" src="/images/article_samples/3.jpg" />
+                                    </a>
+                                    <div class="titlebox">
+                                        <div class="category">Article Category</a></div>
+                                        <h1 class="title">Article Title</h1>
+                                    </div>
+                                </article>
+                            </div>
+                            <div class="col-md-6">
+                                <article class="articlebox medium">
+                                    <a href="#">
+                                        <img class="image" src="/images/article_samples/4.jpg" />
+                                    </a>
+                                    <div class="titlebox">
+                                        <div class="category">Article Category</a></div>
+                                        <h1 class="title">Article Title</h1>
+                                    </div>
+                                </article>
+                            </div>
+                            <div class="col-md-6">
+                                <article class="articlebox medium">
+                                    <a href="#">
+                                        <img class="image" src="/images/article_samples/5.jpg" />
+                                    </a>
+                                    <div class="titlebox">
+                                        <div class="category">Article Category</a></div>
+                                        <h1 class="title">Article Title</h1>
+                                    </div>
+                                </article>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <section id="SecondarySection">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <article class="articlebox">
+                                <a href="#">
+                                    <img class="image" src="/images/article_samples/6.jpg" />
+                                </a>
+                                <div class="titlebox">
+                                    <div class="category">Article Category</a></div>
+                                    <h1 class="title">Article Title</h1>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="col-md-3">
+                            <article class="articlebox">
+                                <a href="#">
+                                    <img class="image" src="/images/article_samples/7.jpg" />
+                                </a>
+                                <div class="titlebox">
+                                    <div class="category">Article Category</a></div>
+                                    <h1 class="title">Article Title</h1>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="col-md-3">
+                            <article class="articlebox">
+                                <a href="#">
+                                    <img class="image" src="/images/article_samples/8.jpg" />
+                                </a>
+                                <div class="titlebox">
+                                    <div class="category">Article Category</a></div>
+                                    <h1 class="title">Article Title</h1>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="col-md-3">
+                            <article class="articlebox">
+                                <a href="#">
+                                    <img class="image" src="/images/article_samples/9.jpg" />
+                                </a>
+                                <div class="titlebox">
+                                    <div class="category">Article Category</a></div>
+                                    <h1 class="title">Article Title</h1>
+                                </div>
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </section>
+        
     </body>
     <footer class="text-center">
         <a href="https://github.com/fmarostica/news-manager">
             <img src="/images/GitHub-Mark-Light-32px.png" alt="Follow in GitHub.com" />
-            <br/>
             <span>Follow the project on GitHub.com</span>
         </a>
         <div class="breaking-news-ticker" id="newsTicker1">
