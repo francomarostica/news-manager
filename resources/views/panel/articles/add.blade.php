5e7e5b28a7d838c7592ae20d03d0988d691d44f7
fmarostica <franco-marostica@outlook.com>
2019-03-13 21:29:34 -0300

    Added preview image on articles add form

--- resources/views/panel/articles/add.blade.php
+++ resources/views/panel/articles/add.blade.php
@@ -4,25 +4,38 @@
     <form id="frmArticle" action="/panel/articles" method="POST" enctype="multipart/form-data" >
         @csrf
         <input type="hidden" value="{{ csrf_token() }}" id="token">
+        <input id="btnFile" name="image" type="file" style="display:none">
         <div class="row">
-            <div class="col-md-9">
-                <label for="txtTitle">Título</label>
-                <input id="txtTitle" name="title" type="text" class="form-control" value="" />
-            </div>
             <div class="col-md-3">
-                    <label for="txtCategory">Categoria</label>
-                    <select id="txtCategory" name="category_id" type="text" class="form-control">
-                    @foreach ($categories as $category)
-                        <option value="{{ $category->url }}">{{ $category->title }}</option>
-                    @endforeach
-                    </select>
+                <div class="image-form-container">
+                    <img id="article-image" class="image-form" src="/images/logo.svg" />
+                    <button id="addImgButton" type="button" class="btn btn-primary">
+                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                            <path fill="#fff" d="M4,4H7L9,2H15L17,4H20A2,2 0 0,1 22,6V18A2,2 0 0,1 20,20H4A2,2 0 0,1 2,18V6A2,2 0 0,1 4,4M12,7A5,5 0 0,0 7,12A5,5 0 0,0 12,17A5,5 0 0,0 17,12A5,5 0 0,0 12,7M12,9A3,3 0 0,1 15,12A3,3 0 0,1 12,15A3,3 0 0,1 9,12A3,3 0 0,1 12,9Z" />
+                        </svg>
+                    </button>
                 </div>
-            <div class="col-md-12 py-2">
-                <label for="txtDescription">Description</label>
-                <textarea id="txtDescription" name="description" type="text" class="form-control"></textarea>
             </div>
-            <div class="col-md-12 py-2">
-                <input id="btnFile" name="image" type="file">
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-9">
+                        <label for="txtTitle">Título</label>
+                        <input id="txtTitle" name="title" type="text" class="form-control" value="" />
+                    </div>
+                    <div class="col-md-3">
+                            <label for="txtCategory">Categoria</label>
+                            <select id="txtCategory" name="category_id" type="text" class="form-control">
+                            @foreach ($categories as $category)
+                                <option value="{{ $category->url }}">{{ $category->title }}</option>
+                            @endforeach
+                            </select>
+                        </div>
+                    <div class="col-md-12 py-2">
+                        <label for="txtDescription">Description</label>
+                        <textarea id="txtDescription" name="description" type="text" class="form-control"></textarea>
+                    </div>
+                        
+                </div>
             </div>
         </div>
         <div class="text-centered py-2">
@@ -32,6 +45,13 @@
         </div>
     </form>
     <script>
+        $("#addImgButton").click(function(e){
+            $("#btnFile").click();
+        });
+        $("#btnFile").change(function(){
+            preview_image(this);
+        });
+
         $("#btnSave").click(function(e){
             e.preventDefault();
             var token = $("#token").val();
@@ -74,11 +94,10 @@
         });
 
         function preview_image(input){
-            var $this = this;
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
                 reader.onload = function(e) {
-                    //$this.element.find(".img-cover").prop("src", e.target.result);
+                    $("#article-image").prop("src", e.target.result);
                 }
                 reader.readAsDataURL(input.files[0]);        
             }
